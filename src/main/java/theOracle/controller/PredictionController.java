f239998d65f41856ba73b60ada8f407fa3ed26fd
cyb3r-shad0w <73255629+cyb3r-shad0w@users.noreply.github.com>
2024-06-27 19:00:29 +0200

    mod

--- src/main/java/theOracle/controller/PredictionController.java
+++ src/main/java/theOracle/controller/PredictionController.java
@@ -24,14 +24,29 @@ public class PredictionController {
 	public String dataReader(Model model,
 							@RequestParam("quotaB1") String quotaB1,
 							@RequestParam("quotaBX") String quotaBX,
-							@RequestParam("quotaB2") String quotaB2
+							@RequestParam("quotaB2") String quotaB2,
+							@RequestParam("partiteTotCasa") String partiteTotCasa,
+							@RequestParam("partiteTotOspite") String partiteTotOspite,
+							@RequestParam("vittorieTotaliCasa") String vittorieTotaliCasa,
+							@RequestParam("sconfitteTotaliCasa") String sconfitteTotaliCasa,
+							@RequestParam("vittorieTotaliOspite") String vittorieTotaliOspite,
+							@RequestParam("sconfitteTotaliOspite") String sconfitteTotaliOspite
 							) {
+		//passo 1
 		String [] probabilitaImplicita= service.probabilitaImplicita(quotaB1, quotaBX, quotaB2);
 		model.addAttribute("marketValueB1", probabilitaImplicita[0]);
 		model.addAttribute("marketValueBX", probabilitaImplicita[1]);
 		model.addAttribute("marketValueB2", probabilitaImplicita[2]);
 		model.addAttribute("aggio", probabilitaImplicita[3]);
-	
+
+		//passo 2
+		String[] probabilitaPartiteTotali = service.probabilitaPartiteTotali(partiteTotCasa, partiteTotOspite,vittorieTotaliCasa,
+																			sconfitteTotaliCasa,vittorieTotaliOspite,
+																			sconfitteTotaliOspite);
+		model.addAttribute("percPartiteTot1", probabilitaPartiteTotali[0]);
+		model.addAttribute("percPartiteTotX", probabilitaPartiteTotali[1]);
+		model.addAttribute("percPartiteTot2", probabilitaPartiteTotali[2]);
+
 		return "result";
 	}
 	
--- src/main/java/theOracle/service/PredictionService.java
+++ src/main/java/theOracle/service/PredictionService.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Service;
 public class PredictionService {
 
 
-	public String[] probabilitaImplicita(String quotaB1,String quotaBX,String quotaB2) {// PASSO 1
+	public String[] probabilitaImplicita(String quotaB1,String quotaBX,String quotaB2)
+	{// PASSO 1
 		
 		Double marketValueB1 = 100 / Double.valueOf(quotaB1);
 		Double marketValueBX = 100 / Double.valueOf(quotaBX);
@@ -18,6 +19,22 @@ public class PredictionService {
 		result[2]=Double.toString(marketValueB2);
 		result[3]=Double.toString(aggio);
 		return result;
+	}
+
+	public String[] probabilitaPartiteTotali(String partiteTotCasa, String partiteTotOspite,
+											 String vittorieTotaliCasa, String sconfitteTotaliCasa,
+											 String vittorieTotaliOspite, String sconfitteTotaliOspite)
+	{// PASSO 2
+		Double partitetotali =  Double.valueOf(partiteTotCasa) +  Double.valueOf(partiteTotOspite);
+		Double percPartiteTot1 = ((Double.valueOf(vittorieTotaliCasa) + Double.valueOf(sconfitteTotaliOspite)) / partitetotali)*100;
+		Double percPartiteTot2 = ((Double.valueOf(vittorieTotaliOspite) + Double.valueOf(sconfitteTotaliCasa)) / partitetotali)*100;
+		Double percPartiteTotX = 100 - (percPartiteTot1 + percPartiteTot2);
+		String [] result = new String [3];
+		result[0]= Double.toString(percPartiteTot1);
+		result[1]= Double.toString(percPartiteTotX);
+		result[2]= Double.toString(percPartiteTot2);
+		return result;
+
 	}
 
 }
